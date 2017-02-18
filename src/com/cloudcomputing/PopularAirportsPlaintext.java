c525824d2e3d908bd9d50b7f0b9a97c376fc3327
gitaroktato <gitaroktato@gmail.com>
2017-02-18 20:59:09 +0100

    Added plaintext solution as zip files have to fit into memory

--- src/com/cloudcomputing/PopularAirportsPlaintext.java
+++ src/com/cloudcomputing/PopularAirportsPlaintext.java
@@ -6,7 +6,6 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.BytesWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
@@ -17,15 +16,14 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 import java.io.IOException;
-import java.util.StringTokenizer;
 import java.util.regex.Pattern;
 
 /**
  * Created by sniper on 2017.02.13..
  */
-public class PopularAirports {
+public class PopularAirportsPlaintext {
 
-    private static final Log LOG = LogFactory.getLog(PopularAirports.class);
+    private static final Log LOG = LogFactory.getLog(PopularAirportsPlaintext.class);
 
 
     /**
@@ -33,24 +31,14 @@ public class PopularAirports {
      * the text and then applies the simple WordCount algorithm.
      */
     public static class MyMapper
-            extends Mapper<Text, Text, Text, IntWritable> {
+            extends Mapper<Object, Text, Text, IntWritable> {
         private final static IntWritable one = new IntWritable(1);
         public static final int ORIGIN_AT = 6;
         public static final int DESTINATION_AT = 14;
         private Text word = new Text();
 
-        public void map(Text key, Text value, Context context)
+        public void map(Object key, Text value, Context context)
                 throws IOException, InterruptedException {
-            // NOTE: the filename is the *full* path within the ZIP file
-            // e.g. "subdir1/subsubdir2/Ulysses-18.txt"
-            String filename = key.toString();
-            LOG.info("map: " + filename);
-
-            // We only want to process .txt files
-            if (!filename.endsWith(".csv"))
-                return;
-
-            // Prepare the content
 
             // Tokenize the content
             Pattern.compile("\n", Pattern.MULTILINE)
@@ -95,25 +83,18 @@ public class PopularAirports {
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
         // Standard stuff
-        Job job = Job.getInstance(conf, PopularAirports.class.getName());
-        job.setJarByClass(PopularAirports.class);
+        Job job = Job.getInstance(conf, PopularAirportsPlaintext.class.getName());
+        job.setJarByClass(PopularAirportsPlaintext.class);
         job.setMapperClass(MyMapper.class);
         job.setCombinerClass(MyReducer.class);
         job.setReducerClass(MyReducer.class);
 
-// Hello there ZipFileInputFormat!
-        job.setInputFormatClass(ZipFileInputFormat.class);
-        job.setOutputFormatClass(TextOutputFormat.class);
-
-// The output files will contain "Word [TAB] Count"
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
 
-// We want to be fault-tolerant
-        ZipFileInputFormat.setLenient(true);
-        ZipFileInputFormat.setInputPaths(job, new Path(args[0]));
-        TextOutputFormat.setOutputPath(job, new Path(args[1]));
-//
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
         job.waitForCompletion(true);
     }
 }
