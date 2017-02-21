2069a096ae8e490d967e3ea3e75b1b18d4baeac0
gitaroktato <gitaroktato@gmail.com>
2017-02-21 21:58:53 +0100

    Average arrival delays

---
+++ src/com/cloudcomputing/AverageDelays.java
@@ -0,0 +1,92 @@
+package com.cloudcomputing;
+
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+import java.util.stream.Stream;
+
+/**
+ * This uses airline_ontime data to sum average on-time arrival times.
+ */
+public class AverageDelays {
+
+    private static final Log LOG = LogFactory.getLog(AverageDelays.class);
+
+    public static class MyMapper
+            extends Mapper<Object, Text, Text, DoubleWritable> {
+        public static final int CARRIER_ID = 8;
+        public static final int ARRIVAL_DELAY = 38;
+        private Text carrierId = new Text();
+        private DoubleWritable arrivalDelay = new DoubleWritable();
+
+        public void map(Object key, Text value, Context context)
+                throws IOException, InterruptedException {
+
+            // Tokenize the content
+            Stream.of(value.toString())
+                    .map(line -> line.split(","))
+                    .filter(tokens -> tokens.length >= ARRIVAL_DELAY)
+                    .forEach(tokens -> {
+                        try {
+                            String carrierValue = tokens[CARRIER_ID].replaceAll("\"", "");
+                            String delayValue = tokens[ARRIVAL_DELAY].replaceAll("\"", "");
+                            carrierId.set(carrierValue);
+                            arrivalDelay.set(Double.parseDouble(delayValue));
+                            context.write(carrierId, arrivalDelay);
+                        } catch (Exception e) {
+                            System.err.println(e);
+                        }
+                    });
+        }
+    }
+
+    /**
+     * Reducer for the ZipFile test, identical to the standard WordCount example
+     */
+    public static class MyReducer
+            extends Reducer<Text, DoubleWritable, Text, Text> {
+        public void reduce(Text key, Iterable<DoubleWritable> values, Context context)
+                throws IOException, InterruptedException {
+            double sum = 0;
+            int count = 0;
+            for (DoubleWritable val : values) {
+                sum += val.get();
+                count++;
+            }
+            double average = sum / count;
+            context.write(key, new Text(String.format("%.2f", average)));
+        }
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        // Standard stuff
+        Job job = Job.getInstance(conf, AverageDelays.class.getName());
+        job.setJarByClass(AverageDelays.class);
+        job.setMapperClass(MyMapper.class);
+        //job.setCombinerClass(MyReducer.class);
+        job.setReducerClass(MyReducer.class);
+
+        job.setMapOutputValueClass(DoubleWritable.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        job.waitForCompletion(true);
+    }
+}
