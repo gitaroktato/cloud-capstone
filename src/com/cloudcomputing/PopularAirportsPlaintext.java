016fd77931c11b547fe1c1f31271a436892b5327
gitaroktato <gitaroktato@gmail.com>
2017-02-21 10:28:21 +0100

    Tokenizing origin-dest with correct results

--- src/com/cloudcomputing/PopularAirportsPlaintext.java
+++ src/com/cloudcomputing/PopularAirportsPlaintext.java
@@ -7,6 +7,7 @@ import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -16,10 +17,13 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 /**
- * Created by sniper on 2017.02.13..
+ * This uses airline_ontime data to sum origin-destination airports
  */
 public class PopularAirportsPlaintext {
 
@@ -31,34 +35,33 @@ public class PopularAirportsPlaintext {
      * the text and then applies the simple WordCount algorithm.
      */
     public static class MyMapper
-            extends Mapper<Object, Text, Text, IntWritable> {
-        private final static IntWritable one = new IntWritable(1);
-        public static final int ORIGIN_AT = 6;
-        public static final int DESTINATION_AT = 14;
+            extends Mapper<Object, Text, Text, LongWritable> {
+        private final static LongWritable one = new LongWritable(1);
+        public static final int ORIGIN_AT = 11;
+        public static final int DESTINATION_AT = 18;
         private Text word = new Text();
 
         public void map(Object key, Text value, Context context)
                 throws IOException, InterruptedException {
 
             // Tokenize the content
-            Pattern.compile("\n", Pattern.MULTILINE)
-                    .splitAsStream(value.toString())
+            Stream.of(value.toString())
                     .map(line -> line.split(","))
-                    .filter( tokens -> tokens.length >= DESTINATION_AT)
+                    .filter(tokens -> tokens.length >= DESTINATION_AT)
                     .forEach(tokens -> {
                         // FROM
                         word.set(tokens[ORIGIN_AT]);
                         try {
                             context.write(word, one);
                         } catch (Exception e) {
-                            System.out.println(e);
+                            System.err.println(e);
                         }
                         // TO
                         word.set(tokens[DESTINATION_AT]);
                         try {
                             context.write(word, one);
                         } catch (Exception e) {
-                            System.out.println(e);
+                            System.err.println(e);
                         }
                     });
         }
@@ -68,14 +71,14 @@ public class PopularAirportsPlaintext {
      * Reducer for the ZipFile test, identical to the standard WordCount example
      */
     public static class MyReducer
-            extends Reducer<Text, IntWritable, Text, IntWritable> {
-        public void reduce(Text key, Iterable<IntWritable> values, Context context)
+            extends Reducer<Text, LongWritable, Text, LongWritable> {
+        public void reduce(Text key, Iterable<LongWritable> values, Context context)
                 throws IOException, InterruptedException {
             int sum = 0;
-            for (IntWritable val : values) {
+            for (LongWritable val : values) {
                 sum += val.get();
             }
-            context.write(key, new IntWritable(sum));
+            context.write(key, new LongWritable(sum));
         }
     }
 
@@ -90,7 +93,7 @@ public class PopularAirportsPlaintext {
         job.setReducerClass(MyReducer.class);
 
         job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(IntWritable.class);
+        job.setOutputValueClass(LongWritable.class);
 
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
