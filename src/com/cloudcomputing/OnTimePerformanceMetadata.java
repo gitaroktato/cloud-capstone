10daa88a41b7c653beca2433a2961e79c2502fd3
gitaroktato <gitaroktato@gmail.com>
2017-02-23 20:47:39 +0100

    Added delay calculation

--- src/com/cloudcomputing/OnTimePerformanceMetadata.java
+++ src/com/cloudcomputing/OnTimePerformanceMetadata.java
@@ -7,6 +7,6 @@ public interface OnTimePerformanceMetadata {
     public static final int ORIGIN_AIRPORT = 11;
     public static final int DESTINATION_AIRPORT = 18;
     public static final int CARRIER_ID = 8;
-    public static final int DEPARTURE_DELAY = 25;
+    public static final int DEPARTURE_DELAY = 27;
     public static final int ARRIVAL_DELAY = 38;
 }
