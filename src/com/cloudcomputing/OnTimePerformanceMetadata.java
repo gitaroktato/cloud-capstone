e4408364d1973947ad7228228d364f20534b7f9d
gitaroktato <gitaroktato@gmail.com>
2017-02-26 21:29:35 +0100

    Cleaning for last query

--- src/com/cloudcomputing/OnTimePerformanceMetadata.java
+++ src/com/cloudcomputing/OnTimePerformanceMetadata.java
@@ -9,7 +9,7 @@ public interface OnTimePerformanceMetadata {
     public static final int DEPARTURE_DATE = 5;
     public static final int CARRIER_ID = 8;
     public static final int FLIGHT_NUM = 10;
-    public static final int SCHEDULED_DEPARTURE_TIME = 26;
+    public static final int SCHEDULED_DEPARTURE_TIME = 25;
     public static final int DEPARTURE_TIME = 26;
     public static final int DEPARTURE_DELAY = 27;
     public static final int ARRIVAL_DELAY = 38;
